13565a066ecdb46b0bf56eaaa36e658231bc7fde
Josh-Likes-Bread <rjoshua@ocadu.ca>
2024-02-13 14:53:57 -0500

    Ali is a wizard

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-function toggleFrame(frameID, src) {
-    var frame = document.getElementById(frameID);
-    var iframe = frame.querySelector("iframe");
-    if (frame.style.display === "none" || frame.style.display === "") {
-        iframe.src = src;
-        frame.style.display = "block";
-    } else {
-        frame.style.display = "none";
-    }
-}
+
 
 
 /*---chatgpt---*/
 
 $(document).ready(function() {
-    $(".webpages").draggable({
-        scroll: false
-    });
+
+
+    $(".frame-link").click(function(){
+
+        iframe = $(this).data("iframe");
+
+            $("<div class='webpages'><button class='closeButton'>&times;</button><iframe src='"+ iframe +"' width='100%' height='600px'></iframe></div>").appendTo("body").draggable({
+                scroll: false
+            });
+
+            $(".closeButton").on("click", function() {
+                $(this).closest(".webpages").remove(); // Hide the iframe when the close button is clicked
+            });
+        
+    })
+
+    $(".webpages")
 
     // Add click event handler for the close button
     $(".closeButton").on("click", function() {
